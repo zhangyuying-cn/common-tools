8b7936ccfa4eab08b65b140c3b09956ec5129b92
zhangyuying <15764338852@163.com>
2023-08-07 13:49:11 +0800

    uuid工具类-单元测试

---
+++ common-tools/src/test/java/com/zyy/tools/UuidTest.java
@@ -0,0 +1,12 @@
+package com.zyy.tools;
+
+
+import com.zyy.tools.util.UuidUtil;
+
+public class UuidTest {
+    public static void main(String[] args) {
+        System.out.println(UuidUtil.uuid8());
+        System.out.println(UuidUtil.uuid16());
+        System.out.println(UuidUtil.uuid32());
+    }
+}
